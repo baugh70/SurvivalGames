b7eea5ddf549eb21040109f03c583d5a4993760a
baugh70 <zakkyzakkba@gmail.com>
2014-02-27 15:00:20 -0500

    Update Lobby.java

--- Lobby.java
+++ Lobby.java
@@ -1,4 +1,39 @@
-public class Lobby {
+public class Lobby implements Listener{
+    ArrayList<Player> players = new ArrayList<>();
     
+    public void onPlayerJoin(PlayerJoinEvent event){
+        Player player = event.getPlayer();
+        
+        players.add(player);
+    }
     
+    public void onPlayerLeave(PlayerLeaveEvent){
+        Player player = event.getPlayer();
+        
+        players.remove(player);
+        
+    }
+    
+    public void onPlayerKick(PlayerKickEvent event){
+        Player player = event.getPlayer();
+        
+        players.remove(player);
+    }
+    
+    public void onPlayerDropItem(PlayerDropItemEvent event){
+        
+        event.setCancelled(true);
+    }
+    
+    public void onPlayerInteract(PlayerInteractEvent event){
+        
+        event.setCancelled(true);
+    }
+    
+    public void onToggleFlightEvent(ToggleFlightEvent event){
+        
+        
+    }
+    
+    public void on
 }
